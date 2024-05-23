a9698f9e70395e7c11751d1c5e18502a60519c62
Justin Lamb <justinlamb@me.com>
2024-05-23 13:18:46 -0700

    first build

---
+++ builds/idle-garden-html5-0.1/gamedata/app/automation.rb
@@ -0,0 +1,84 @@
+# frozen_string_literal: true
+
+require 'app/plant.rb'
+
+# Create new automations for garden
+class Automation
+  attr_accessor :type, :harvest_cooldown, :planter_cooldown, :seller_cooldown
+
+  attr_sprite
+
+  def initialize(type)
+    @type = type
+    @harvest_cooldown = 3
+    @planter_cooldown = 3
+    @seller_cooldown = 3
+  end
+
+  def run(args)
+    reduce_cooldowns
+    case @type
+    when :harvester
+      auto_harvester(args) if @harvest_cooldown <= 0 && args.state.plants.length.positive?
+    when :planter
+      auto_planter(args) if @planter_cooldown <= 0 && args.state.seeds.positive?
+    when :seller
+      auto_seller(args) if @seller_cooldown <= 0 && args.state.harvested_plants.positive?
+    end
+  end
+
+  private
+
+  def reduce_cooldowns
+    @harvest_cooldown -= 1
+    @planter_cooldown -= 1
+    @seller_cooldown -= 1
+  end
+
+  def auto_harvester(args)
+    args.state.plants.each do |plant|
+      next unless plant.stage == 'full_grown' || plant.stage == 'withered'
+
+      plant.harvest(args, plant)
+      @harvest_cooldown = rand(15)
+      break
+    end
+  end
+
+  def auto_planter(args)
+    args.inputs.mouse.x, args.inputs.mouse.y = coord_generator
+    plant = Plant.new(args)
+    args.state.plants << plant
+    args.state.seeds -= 1
+    @planter_cooldown = rand(10)
+  end
+
+  def coord_generator
+    # x 250-980, y 50-620
+    x = rand(980)
+    x += 250 if x < 250
+    y = rand(620)
+    y += 50 if y < 50
+    [x, y]
+  end
+
+  def auto_seller(args)
+    args.state.cash += args.state.harvested_plants * args.state.price[:plant]
+    args.state.harvested_plants = 0
+    @seller_cooldown = rand(10)
+  end
+
+  # DragonRuby required methods
+  def serialize
+    { type: @type, harvest_cooldown: @harvest_cooldown, planter_cooldown: @planter_cooldown,
+      seller_cooldown: @seller_cooldown }
+  end
+
+  def inspect
+    serialize.to_s
+  end
+
+  def to_s
+    serialize.to_s
+  end
+end
---
+++ builds/idle-garden-mac-0.1/Idle Garden.app/Contents/Resources/app/plant.rb
@@ -0,0 +1,95 @@
+# frozen_string_literal: true
+
+# Create new plants in garden
+class Plant
+  attr_accessor :x, :y, :w, :h, :age, :invalid, :path, :stage, :a
+
+  attr_sprite
+
+  # Growth Stages & Rates
+  GROWTH_RATE = 0.1
+  GROWING = 200
+  FULL_GROWN = 400
+  WITHER = 1000
+  WITHER_RATE = 0.4
+  DEATH = 2200
+  SPRITES = { SEED: 'sprites/stages/0seed.png', GROWING: 'sprites/stages/1growing.png',
+              FULL_GROWN: 'sprites/stages/2full_grown.png', WITHERED: 'sprites/stages/3withered.png' }.freeze
+  STAGES = %w[seed growing full_grown withered].freeze
+
+  def initialize(args)
+    @x = args.inputs.mouse.x - 15
+    @y = args.inputs.mouse.y - 15
+    @w = 20
+    @h = 20
+    @age = 0
+    @invalid = occupied(args, [@x, @y, @w, @h])
+    @path = SPRITES[:SEED]
+    @stage = STAGES[0]
+    @a = 255
+  end
+
+  def grow
+    @age += 1
+    set_growth_stage
+    if @age <= FULL_GROWN
+      @w += GROWTH_RATE
+      @h += GROWTH_RATE
+    elsif @age >= WITHER && @age < DEATH
+      @a -= WITHER_RATE
+    elsif @age >= DEATH
+      @invalid = true
+    end
+  end
+
+  # Harvest plant if correct stage
+  def harvest(args, plant)
+    if plant.stage == 'full_grown'
+      args.state.harvested_plants += 1
+      plant.invalid = true
+    elsif plant.stage == 'withered'
+      args.state.seeds += rand(10)
+      plant.invalid = true
+    end
+  end
+
+  private
+
+  def set_growth_stage
+    if @age >= GROWING && @age < FULL_GROWN
+      @path = SPRITES[:GROWING]
+      @stage = STAGES[1]
+    elsif @age >= FULL_GROWN && @age < WITHER
+      @path = SPRITES[:FULL_GROWN]
+      @stage = STAGES[2]
+    elsif @age >= WITHER && @age < DEATH
+      @path = SPRITES[:WITHERED]
+      @stage = STAGES[3]
+    end
+  end
+
+  # sets @invalid to false if not occupied, attemps to harvest plant at location if occupied
+  def occupied(args, new_plant)
+    args.state.plants.each do |plant|
+      next unless args.geometry.intersect_rect?(plant, new_plant)
+
+      harvest(args, plant)
+      return true
+    end
+    false
+  end
+
+
+  # DragonRuby required methods
+  def serialize
+    { w: @w, h: @h, x: @x, y: @y, age: @age, invalid: @invalid, path: @path, stage: @stage, a: @a }
+  end
+
+  def inspect
+    serialize.to_s
+  end
+
+  def to_s
+    serialize.to_s
+  end
+end
