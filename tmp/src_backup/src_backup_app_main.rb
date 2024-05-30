e83d8c2b4d1f6b543357a6e957670b0f6d70f14a
Justin Lamb <justinlamb@me.com>
2024-05-30 11:56:02 -0700

    todos

--- tmp/src_backup/src_backup_app_main.rb
+++ tmp/src_backup/src_backup_app_main.rb
@@ -1,84 +1,203 @@
-def tick args
-  args.state.tile_size     = 80
-  args.state.player_speed  = 4
-  args.state.player      ||= tile(args, 7, 3, 0, 128, 180)
-  generate_map args
-  #press j to plant a green onion
-  if args.inputs.keyboard.j
-  #change this part you can change what you want to plant
-   args.state.walls << tile(args, ((args.state.player.x+80)/args.state.tile_size), ((args.state.player.y)/args.state.tile_size), 255, 255, 255)
-   args.state.plants << tile(args, ((args.state.player.x+80)/args.state.tile_size), ((args.state.player.y+80)/args.state.tile_size), 0, 160, 0)
+# frozen_string_literal: true
+
+require 'app/plant.rb'
+require 'app/automation.rb'
+
+# Total Interactive Area
+def in_bounds(args)
+  args.inputs.mouse.x <= 1280 &&
+    args.inputs.mouse.x >= 0 &&
+    args.inputs.mouse.y <= 720 &&
+    args.inputs.mouse.y >= 0
+end
+
+# Area available for plants
+def in_garden(args)
+  args.inputs.mouse.x <= 980 &&
+    args.inputs.mouse.x >= 250 &&
+    args.inputs.mouse.y <= 620 &&
+    args.inputs.mouse.y >= 50
+end
+
+# helper method to create a button
+def new_button(id, x, y, text)
+  width = 100
+  height = 50
+  entity = {
+    id: id,
+    rect: { x: x, y: y, w: width, h: height }
+  }
+
+  entity[:primitives] = [
+    { x: x, y: y, w: width, h: height }.border!,
+    { x: x + 5, y: y + 30, text: text, size_enum: -4 }.label!
+  ]
+  entity
+end
+
+# helper method for determining if a button was clicked
+def button_clicked?(args, button)
+  return false unless args.inputs.mouse.click
+
+  args.inputs.mouse.point.inside_rect? button[:rect]
+end
+
+# TODO: Review borders -> being redrawn on every tick
+# TODO: Change background to .png -> flappy dragon example
+def tick(args)
+  args.outputs.solids << [200, 0, 1280, 720, 138, 185, 54] # grass background [x,y,w,h,r,g,b]
+  args.outputs.solids << [250, 50, 980, 620, 170, 129, 56] # dirt background
+  # args.outputs.static_borders << { x: 0, y: 0, w: 1280, h: 720 }
+  # args.outputs.static_borders << { x: 0, y: 1, w: 1280, h: 0 }
+  args.state.plants ||= []
+  args.state.seeds ||= 5
+  args.state.harvested_plants ||= 0
+  args.state.cash ||= 5
+  args.state.price = { seed: 5, plant: 10, harvester: 150, planter: 150, seller: 50 }
+  args.state.auto_planters ||= []
+  args.state.auto_harvesters ||= []
+  args.state.auto_sellers ||= []
+  args.state.counter ||= 0
+
+  args.state.counter += 1
+
+  # Buy Seeds Button
+  args.state.buy_seed_button ||= new_button :buy_seed, 0, 0, "Seed (#{args.state.price[:seed]})"
+  args.outputs.primitives << args.state.buy_seed_button[:primitives]
+
+  # check if the click occurred and buys seeds if enough money
+  if args.inputs.mouse.click && button_clicked?(args, args.state.buy_seed_button) && !(args.state.cash - args.state.price[:seed] < 0)
+    args.state.seeds += 1
+    args.state.cash -= args.state.price[:seed]
   end
-  # Adds walls, background, and player to args.outputs.solids so they appear on screen
-  args.outputs.solids << [0,0,1280,720, 237,189,101]
-  args.outputs.sprites << [0, 0, 1280, 720, 'sprites/background.png']
-  args.outputs.solids << args.state.walls
-  args.outputs.solids << args.state.player
-  args.outputs.solids << args.state.plants
-  args.outputs.labels << [320, 640, "press J to plant", 3, 1, 255, 0, 0, 200]
 
-  move_player args, -1,  0 if args.inputs.keyboard.left # x position decreases by 1 if left key is pressed
-  move_player args,  1,  0 if args.inputs.keyboard.right # x position increases by 1 if right key is pressed
-  move_player args,  0,  1 if args.inputs.keyboard.up # y position increases by 1 if up is pressed
-  move_player args,  0, -1 if args.inputs.keyboard.down # y position decreases by 1 if down is pressed
-end
+  # Sell Harvest Button
+  args.state.sell_button ||= new_button :sell, 100, 0, 'Sell'
+  args.outputs.primitives << args.state.sell_button[:primitives]
 
-# Sets position, size, and color of the tile
-def tile args, x, y, *color
-  [x * args.state.tile_size, # sets definition for array using method parameters
-   y * args.state.tile_size, # multiplying by tile_size sets x and y to correct position using pixel values
-   args.state.tile_size,
-   args.state.tile_size,
-   *color]
-end
+  # check if the click occurred and sells harvest
+  if args.inputs.mouse.click && button_clicked?(args, args.state.sell_button) && !(args.state.harvested_plants.negative?)
+    args.state.cash += args.state.harvested_plants * args.state.price[:plant]
+    args.state.harvested_plants = 0
+  end
 
-# Creates map by adding tiles to the wall, as well as a goal (that the player needs to reach)
-def generate_map args
-  return if args.state.area
+  # Make Auto Harvester Button
+  args.state.auto_harvester_button ||= new_button :auto_harvester, 0, 50, "Harvester (#{args.state.price[:harvester]})"
+  args.outputs.primitives << args.state.auto_harvester_button[:primitives]
 
-  # Creates the area of the map. There are 9 rows running horizontally across the screen
-  # and 16 columns running vertically on the screen. Any spot with a "1" is not
-  # open for the player to move into (and is green), and any spot with a "0" is available
-  # for the player to move in.
-  args.state.area = [
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,],
-    [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,],
-    [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,],
-    [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,],
-    [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,],
-    [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,],
-    [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ],
-  ].reverse # reverses the order of the area collection
+  # check if the click occurred and creates auto harvester
+  if args.inputs.mouse.click && button_clicked?(args, args.state.auto_harvester_button) && !(args.state.cash - args.state.price[:harvester] < 0)
+    args.state.auto_harvesters << Automation.new(:harvester)
+    args.state.cash -= args.state.price[:harvester]
+  end
 
-  # By reversing the order, the way that the area appears above is how it appears
-  # on the screen in the game. If we did not reverse, the map would appear inverted.
+  # Make Auto Seller Button
+  args.state.auto_seller_button ||= new_button :auto_seller, 100, 50, "Seller (#{args.state.price[:seller]})"
+  args.outputs.primitives << args.state.auto_seller_button[:primitives]
 
-  #The wall starts off with no tiles.
-  args.state.walls = []
-  args.state.plants = []
+  # check if the click occurred and creates auto seller
+  if args.inputs.mouse.click && button_clicked?(args, args.state.auto_seller_button) && !(args.state.cash - args.state.price[:seller] < 0)
+    args.state.auto_sellers << Automation.new(:seller)
+    args.state.cash -= args.state.price[:seller]
+  end
 
-  # If v is 1, a green tile is added to args.state.walls.
-  # If v is 2, a black tile is created as the goal.
-  args.state.area.map_2d do |y, x, v|
-    if    v == 1
-      args.state.walls << tile(args, x, y, 255, 160, 156) # green tile
+  # Make Auto Planter Button
+  args.state.auto_planter_button ||= new_button :auto_planter, 0, 100, "Planter (#{args.state.price[:planter]})"
+  args.outputs.primitives << args.state.auto_planter_button[:primitives]
+
+  # check if the click occurred and creates auto planter
+  if args.inputs.mouse.click && button_clicked?(args, args.state.auto_planter_button) && !(args.state.cash - args.state.price[:planter] < 0)
+    args.state.auto_planters << Automation.new(:planter)
+    args.state.cash -= args.state.price[:planter]
+  end
+
+  # Place or harvest plants in garden
+  if args.inputs.mouse.click && in_garden(args)
+    new_plant = Plant.new(args)
+
+    if args.state.seeds.positive? && !new_plant.invalid
+      args.state.plants << new_plant
+      args.state.seeds -= 1
     end
   end
-end
 
-# Allows the player to move their box around the screen
-def move_player args, *vector
-  box = args.state.player.shift_rect(vector) # box is able to move at an angle
+  # Remove invalid plants
+  args.state.plants.reject!(&:invalid)
 
-  # If the player's box hits a wall, it is not able to move further in that direction
-  return if args.state.walls
-                .any_intersect_rect?(box)
+  # Grow plants
+  args.state.plants.each(&:grow)
 
-  # Player's box is able to move at angles (not just the four general directions) fast
-  args.state.player =
-    args.state.player
-        .shift_rect(vector.x * args.state.player_speed, # if we don't multiply by speed, then
-                    vector.y * args.state.player_speed) # the box will move extremely slow
+  # Run automations at regular intervals (2.5 seconds)
+  if args.state.counter >= 30 * 2.5
+    # Run auto harvesters
+    args.state.auto_harvesters.each { |harvester| harvester.run(args) }
+
+    # Run auto sellers
+    args.state.auto_sellers.each { |seller| seller.run(args) }
+
+    # Run auto planters
+    args.state.auto_planters.each { |planter| planter.run(args) }
+
+    # Reset counter
+    args.state.counter = 0
+  end
+
+  # Render sprites
+  args.outputs.sprites << [args.state.plants]
+
+  # Display number of seeds
+  args.outputs.labels << {
+    x: 5,
+    y: args.grid.h - 20,
+    text: "Seeds: #{args.state.seeds}",
+    size_px: 22
+  }
+
+  # Display number of plants
+  args.outputs.labels << {
+    x: 5,
+    y: args.grid.h - 40,
+    text: "Growing: #{args.state.plants.length}",
+    size_px: 22
+  }
+
+  # Display harvested plants
+  args.outputs.labels << {
+    x: 5,
+    y: args.grid.h - 60,
+    text: "Harvested: #{args.state.harvested_plants}",
+    size_px: 22
+  }
+
+  # Display cash
+  args.outputs.labels << {
+    x: 5,
+    y: args.grid.h - 80,
+    text: "Cash: #{args.state.cash}",
+    size_px: 22
+  }
+
+  # Display auto harvesters
+  args.outputs.labels << {
+    x: 5,
+    y: args.grid.h - 100,
+    text: "Harvesters: #{args.state.auto_harvesters.length}",
+    size_px: 22
+  }
+
+  # Display auto planters
+  args.outputs.labels << {
+    x: 5,
+    y: args.grid.h - 120,
+    text: "Planters: #{args.state.auto_planters.length}",
+    size_px: 22
+  }
+
+  # Display auto sellers
+  args.outputs.labels << {
+    x: 5,
+    y: args.grid.h - 140,
+    text: "Sellers: #{args.state.auto_sellers.length}",
+    size_px: 22
+  }
 end
