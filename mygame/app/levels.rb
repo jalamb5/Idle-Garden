16e1ca63e3d9284e9dd09117056a51db4c806773
Justin Lamb <justinlamb@me.com>
2024-08-03 13:09:22 -0700

    scaffold new unlock logic

--- mygame/app/levels.rb
+++ mygame/app/levels.rb
@@ -15,18 +15,35 @@ class Level
   end
 
   def tick(args)
+    update(args)
+    apply_unlocks(args)
+  end
+
+  private
+
+  def update(args)
     case args.state.game_state.score
     when 0..100
       @current_level = 1
     when 101..200
       @current_level = 2
-      args.state.game_state.unlock_buttons[:auto_planter] = Button.new(:auto_planter, 0, 100, "Planter (#{args.state.game_state.price[:planter]})")
     end
   end
-  def update(level)
-    @current_level = level
+
+  # Apply unlock changes to all levels even if player leapfrogs a level
+  def apply_unlocks(args)
+    (1..@current_level).each do |level|
+      unlock_level(args, level)
+    end
   end
 
+  def unlock_level(args, level)
+    case level
+    when 2
+      args.state.game_state.unlock_buttons[:auto_planter] ||=
+        Button.new(:auto_planter, 0, 100, "Planter (#{args.state.game_state.price[:planter]})")
+    end
+  end
 
   # DragonRuby required methods
   def serialize
