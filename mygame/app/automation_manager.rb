cbcb436434a693fca581fbb0daf7de5475acf22b
Justin Lamb <justinlamb@me.com>
2024-08-12 14:37:22 -0700

    comments

--- mygame/app/automation_manager.rb
+++ mygame/app/automation_manager.rb
@@ -7,7 +7,7 @@ require 'app/automation.rb'
 
 # Handle running of automations
 class AutomationManager
-  attr_accessor :auto_planters, :auto_harvesters, :auto_sellers, :counter
+  attr_accessor :auto_planters, :auto_harvesters, :auto_sellers
 
   def initialize
     @auto_planters = []
@@ -31,17 +31,17 @@ class AutomationManager
     automations.each { |automation| automation.run(args) }
   end
 
-  def reconstruct_automations(type, automations)
-    attributes = %i[type harvest_cooldown planter_cooldown seller_cooldown]
+  # def reconstruct_automations(type, automations)
+  #   attributes = %i[type harvest_cooldown planter_cooldown seller_cooldown]
 
-    automations.map! do |automation|
-      new_automation = Automation.new(type)
-      attributes.each do |attr|
-        new_automation.send("#{attr}=", automation.send(attr))
-      end
-      new_automation
-    end
-  end
+  #   automations.map! do |automation|
+  #     new_automation = Automation.new(type)
+  #     attributes.each do |attr|
+  #       new_automation.send("#{attr}=", automation.send(attr))
+  #     end
+  #     new_automation
+  #   end
+  # end
 
   private
 
