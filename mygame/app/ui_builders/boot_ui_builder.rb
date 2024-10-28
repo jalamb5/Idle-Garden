59883a4bea3f8f51e35fb174b501305407762a65
Justin Lamb <justinlamb@me.com>
2024-10-28 11:28:20 -0700

    rmv save/load btns

--- mygame/app/ui_builders/boot_ui_builder.rb
+++ mygame/app/ui_builders/boot_ui_builder.rb
@@ -12,8 +12,10 @@ class BootUIBuilder
 
   def initialize
     @splash_screen = SplashBuilder.new
-    @buttons = [Button.new(:start, [540, 360], 'Start', [200, 50], :opaque),
-                Button.new(:load_save, [540, 260], 'Load Save', [200, 50], :opaque)]
+    @buttons = [
+      Button.new(:start, [540, 360], 'Start', [200, 50], :opaque),
+      # Button.new(:load_save, [540, 260], 'Load Save', [200, 50], :opaque)
+    ]
   end
 
   def tick(args)
--- mygame/app/ui_builders/pause_builder.rb
+++ mygame/app/ui_builders/pause_builder.rb
@@ -17,12 +17,14 @@ class Pause
   def pause_buttons(args)
     {
       pause_game: Button.new(:pause_game, [400, 360], 'Return to Garden', [200, 50], :opaque),
-      save: Button.new(:save, [700, 360], 'Save', [200, 50], :opaque),
-      quit: Button.new(:quit, [540, 60], 'Quit Game', [200, 50], :opaque),
+      quit: Button.new(:quit, [700, 360], 'Quit Game', [200, 50], :opaque),
       mute_sfx: create_mute_button(:mute_sfx, 400, args.state.boot.sound_manager.sfx_gain, 'Unmute Sound Effects',
                                    'Mute Sound Effects'),
       mute_music: create_mute_button(:mute_music, 700, args.state.boot.sound_manager.music_gain, 'Unmute Music',
                                      'Mute Music')
+      # Original Save and Quit for when Save/Load reimplemented.
+      # save: Button.new(:save, [700, 360], 'Save', [200, 50], :opaque),
+      # quit: Button.new(:quit, [540, 60], 'Quit Game', [200, 50], :opaque),
     }
   end
 
